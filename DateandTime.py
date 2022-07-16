38fc385ff3d521b97fdc338842fc98bb5c535945
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-07-16 14:22:10 +0530

    .

---
+++ DateandTime.py
@@ -0,0 +1,9 @@
+import datetime
+import time 
+#print(time.time())
+#print(time.asctime(time.localtime(time.time())))
+datetime_object = datetime.datetime.now()
+print(datetime_object)
+
+import calendar
+var = calendar.prcal(2023)
---
+++ forloop2.py
@@ -0,0 +1,3 @@
+for num in range(10, 20):
+	if(num%2==0):
+		print(num)
