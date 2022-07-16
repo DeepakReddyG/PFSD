5792cc0397b928d737bc8847cbf6d66c4f458178
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-07-16 13:48:38 +0530

    new files added

---
+++ forloop.py
@@ -0,0 +1,4 @@
+#forloop in python 
+
+for num in range "2000031034": 
+	print('Current Number:', num)
---
+++ marks.py
@@ -0,0 +1,5 @@
+#marks here is list type
+marks = [10, 55, 77, 56, 55, 10] 
+print(type(marks))
+for i in range(len(marks)):
+	print("current loop number:", marks[i])
