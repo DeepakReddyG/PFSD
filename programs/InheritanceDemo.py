4d9e3afdebc2f2d67ae9a6b6d011c5e4deefb202
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-07-21 16:56:36 +0530

    sub folder programs created

---
+++ programs/InheritanceDemo.py
@@ -0,0 +1,8 @@
+from /Users/deepakreddygathpa/Documents/deskCube/II/II-one/PFSD/programs.Addition import Addition
+
+class d(Addition):
+	#obj1 = b()
+	var3 = 1000+Addition.result 
+	#var5 = obj1.var1 + b.var2
+	print(var3)
+	#Oprint(var5) 
