7eaf1d019d3c854de5b5cad36046e48da157f545
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-07-29 15:38:08 +0530

    pytest files added to the module

---
+++ 28-07-2022/three.py
@@ -0,0 +1,4 @@
+import pytest
+@pytest.mark.parametrize("num,output",[(1,11),(2,22),(3,35),(4,44)])
+def test_multiplication_11(num,output): 
+	assert 11*num == output 
--- 28-07-2022/two.py
+++ 28-07-2022/two.py
@@ -1,5 +1,15 @@
 import one
 
 def test_function():
-	result = one.number(input('Enter some number:'))
+	result = one.number(4)
 	assert result == 25
+
+def test_function_2():
+	result = one.number(5)
+	assert result == 25
+def test_function_3():
+	result = one.number(5) 
+	result == 25
+def function_4():
+	result = one.number(5)
+	result == 25
