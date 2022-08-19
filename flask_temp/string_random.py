76b600c42eb7e4ab448557205026b463fef4ad32
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-08-19 15:08:06 +0530

    19-08-2022

--- flask_temp/string_random.py
+++ flask_temp/string_random.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import *
 import random
 import string
 app = Flask(__name__) 
 @app.route('/')
 def random_string():
-	for i in range(3):
-		result_str = ''.join(random.sample(string.ascii_lowercase,8)) 
-		return result_str
+	numb = request.form['numb'] 
+	result_str = ''.join(random.sample(string.ascii_lowercase,int(numb))) 
+	return result_str
 
 if __name__ == '__main__':  
    app.run(debug = True)
