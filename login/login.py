96936b4475f53fc929f40ca717a913e33372096d
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-09-03 13:40:20 +0530

    03-09-2022

--- login/login.py
+++ login/login.py
@@ -1,7 +1,12 @@
 from flask import *
 import sqlite3
-app = Flask(__name__)
 
+
+#create the object of Flask
+app  = Flask(__name__)
+
+
+#creating our routes
 @app.route('/')
 def Index():
     name = 'KL University - CSE [Honors] - Y21 - PFSD - S17'
@@ -15,50 +20,55 @@ def Contact():
 def About():
     return render_template('about.html')
 
-@app.route('/newuser')
+@app.route("/login")
+def my_index_page():
+    return render_template("login.html")
+
+@app.route("/newuser")
 def my_newuser_register_page():
-	return render_template('newuserregister.html')
+    return render_template("newuserregister.html")
 
-@app.route('/registeruser', methods = ['POST','GET'])
-def my_register_user():
-	entered_username = request.form.get('username')
-	entered_password = request.form.get('password')
-	entered_password = entered_password.lower()
-	entered_email = request.form.get('email')
-	entered_mobileno = request.form.get('mobileno')
-	
-	print(entered_username, entered_password, entered_email, entered_mobileno)
-	
-	con = sqlite3.connect("my_database.sqlite3") 
-	cur = con.cursor()
-	my_table_query = 'create table if not exists userstable(name varchar(20),password varchar(15), email varchar(30),mobileno varchar(10))' 
+@app.route("/registeruser", methods=['POST','GET'])
+def my_regiser_user():
+    entered_username = request.form.get("username")
+    entered_password = request.form.get("password")
+    entered_password = entered_password.lower()
+    entered_email = request.form.get("email")
+    entered_mobileno = request.form.get("mobileno")
 
-	cur.execute(my_table_query)
+    print(entered_username, entered_password, entered_email, entered_mobileno)
 
-	cur.execute(f"select email from userstable where email='{entered_email}'")
-	result = cur.fetchone()
-	if result != None:
-		return 'Email Already Exists..Try again'
-	else:
-		my_insert_query = f"insert into userstable"
-		
-	values('{entered_username}','{entered_password}','{entered_email}','{entered_mobileno}')
-	cur.execute(my_insert_query)
-	con.commit()
-	return "user registered Successfully"
+    con = sqlite3.connect("my_database.sqlite3")
 
-@app.route('/loginuser',methods=['POST','GET'])
+    cur = con.cursor()
+
+    my_table_query = "create table if not exists userstable(name varchar(20),password varchar(15),email varchar(30),mobileno varchar(10))"
+    cur.execute(my_table_query)
+
+    cur.execute(f"select email from userstable where email='{entered_email}'")
+    result = cur.fetchone()
+    if result != None:
+        return "Email Already Exists....Try again"
+    else:
+        my_insert_query = f"insert into userstable values('{entered_username}','{entered_password}','{entered_email}','{entered_mobileno}')"
+        cur.execute(my_insert_query)
+        con.commit()
+        return "User Registered Successfully"
+
+@app.route("/loginuser", methods=['POST','GET'])
 def my_login():
-	entered_username = request.form.get("username")
-	entered_password = request.form.get("password")
-	con = sqlite3.connect("my_database.sqlite3")
-	cur = con.cursor()
-	cur.execute(f"select * from userstable where name = '{entered_username}' and password = '{entered_password}' ")
-	result = cur.fetchone()
-	if result is None:
-		return "Invalid user credentials..try again"
-	else:
-		return "login success"
+    entered_username = request.form.get("username")
+    entered_password = request.form.get("password")
+    con = sqlite3.connect("my_database.sqlite3")
+    cur = con.cursor()
+    cur.execute(f"select * from userstable where name='{entered_username}' and password='{entered_password}'")
 
-if __name__ == '__main__':
-	app.run(debug=True)
+    result = cur.fetchone()
+    if result is None:
+        return "Invalid User Credentials....try again"
+    else:
+        return "login success"
+
+#run flask app
+if name == "main":
+    app.run(debug=True)
