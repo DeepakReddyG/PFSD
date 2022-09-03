da41f4ed5718eae83850998b149fc12fb614797b
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-09-03 13:49:45 +0530

    03-09-2022

--- flask_email/emails.py
+++ flask_email/emails.py
@@ -6,16 +6,16 @@ mail= Mail(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = 'premcharanpremcharan335@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wxwxmqsfyklajrji'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 @app.route("/")
 def index():
-   msg = Message('', sender = '', recipients = 
-[''])
+   msg = Message('2100030857', sender = 'premcharanpremcharan335@gmail.com', 
+recipients = ['deepak@kluniversity.in'])
    msg.body = "2100032221 done with sending email through flask! "
    mail.send(msg)
    return "Sent"
