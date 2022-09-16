31fecd8eb97c4bc880379543d97ce5ffb33ff119
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2022-09-16 14:00:44 +0530

    new push

--- sqlalchemy/sqlalchemy_example1.py
+++ sqlalchemy/sqlalchemy_example1.py
@@ -8,6 +8,25 @@ myData = Table(
 	Column('father_name',String),
 	Column('mother_name',String)
 )	
-conn  = engine.connect() 
+meta.create_all(engine)
+ins  = myData.insert().values(id = 124,name = 'Deepak', father_name = 'saireddy',mother_name='chandrakala')
+ins1 = [
+	[122,"Deepak2", "saireddy2","chandrakala2"],
+	[123,"Deepak3", "saireddy3","chandrakala3"],
+	[124,"brek","thomaz","julia"],
+	[125,"john","alex","mary"]
+]
+engine.execute(myData.insert().values(ins1)) 
+conn = engine.connect()
 x = myData.select() 
+result = conn.execute(x)
+for row in result:
+	print (row)
 
+b = myData.update().where(myData.c.name=='brek').values(name='dyvtva') 
+conn.execute(b) 
+y = myData.select() 
+conn.execute(y).fetchall()
+result = conn.execute(y) 
+for row in result:
+	print(row)
